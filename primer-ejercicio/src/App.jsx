04aab1ca435b4eb637959a6d120b687f25c7642e
dev3r3nze <dev3r3nze@gmail.com>
2023-03-24 12:06:18 +0100

    first commit

---
+++ primer-ejercicio/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '../public/vite.svg'
+import './App.css'
+
+function App () {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        
+      </header>
+      </div>
+    )
+}
+
+export default App
