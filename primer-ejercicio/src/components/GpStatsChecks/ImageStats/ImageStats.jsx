55198e16a15aaeef1582139d598c47a5d8169cff
dev3r3nze <dev3r3nze@gmail.com>
2023-04-25 08:55:18 +0200

    Pequeños ajustes

--- primer-ejercicio/src/components/GpStatsChecks/ImageStats/ImageStats.jsx
+++ primer-ejercicio/src/components/GpStatsChecks/ImageStats/ImageStats.jsx
@@ -13,7 +13,13 @@ const ImageStats = (props) =>  {
         return (
             <div className='row justify-content-center'>
                 <img src={props.image} alt="" className='imgStat'/>
-                {data && <Stats opiniones={data.social.gp_socials[0].n_reviews} puntos={data.social.gp_socials[0].gp_stars} estudiantes={`+${data.global.gp_social_variable[0].gp_number}`}/>}
+                {data ? 
+                <Stats opiniones={data.social.gp_socials[0].n_reviews} 
+                puntos={data.social.gp_socials[0].gp_stars} 
+                estudiantes={`+${data.global.gp_social_variable[0].gp_number}`}/>
+                : 
+                <div><p>Loading...</p></div>
+            }
             </div>
         );
 }
